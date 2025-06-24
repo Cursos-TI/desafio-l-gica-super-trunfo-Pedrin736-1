b4a44e0306f2c8bfa1d5c09be7cb69b358c2a611
Pedrin736 <pl145251@gmail.com>
2025-06-24 16:01:20 +0000

    carta1

--- países.c
+++ países.c
@@ -6,7 +6,7 @@
 char estado = "Bahia" [10]; //Bahia
 char codigo = "A01" [7]; //A01
 char nomeCidade = "Salvador" [50]; //Salvador
-int numero_habitantes = 200000; //200000 
+int populacao = 200000; //200000 
 float areaTotal = 2000.000;
 float pib1 = 5000.0000;
 int pontos_turisticos1 = 50; // 50
@@ -15,29 +15,29 @@ int pontos_turisticos1 = 50; // 50
 char estado2 = "Riodejaneiro" [15]; //Riodejaneiro
  char codigo2 = "B02" [7]; // B02
  char nomeCidade2 = "Queimados" [20]; // Queimados
- int numero_habitantes2 = 4000; // 40000
+ int populacao2 = 4000; // 40000
  float areaTotal2 = 20.000;
  float pib2 = 500.000;
  int  pontos_turisticos2 = 23; // 23
     
     // Calculo de densidade Populacional e PIB per capita
   
-    float densidade1 = numero_habitantes / areaTotal;
-    float densidade = numero_habitantes2 / areaTotal2;
+    float densidade1 = populacao/ areaTotal;
+    float densidade = populacao2 / areaTotal2;
 
-    float pib_per_capita1 = pib1 / numero_habitantes;
-    float pib_per_capita2 = pib2 /  numero_habitantes2;
+    float pib_per_capita1 = pib1 / populacao;
+    float pib_per_capita2 = pib2 /  populacao2;
 
    // Atributo para comparacao (numero_habitantes)
    
-    int numero_habitantes;
-    int numero_habitantes2;
+    int populacao;
+    int populacao2;
     int valor1 = 200000;
     int valor2 = 4000; 
 
     //Dados da carta
     
-    printf("Comparação das cartas: %s &numero_habitantes %d:\n", numero_habitantes);
+    printf("Comparação das cartas: %s populacao %d:\n", populacao);
     printf("Carta - %s (%s) %d\n", "nome1", "estado1", "valor1");
     printf("Carta - %s (%s) %d\n", "nome2, estado2, valor2");
 
